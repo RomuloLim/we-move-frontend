ca27aee51503dd659935e2829acfe6f42c8e0bce
pedroluanx28 <macielpedro794@gmail.com>
2025-10-29 23:11:47 -0300

    implement vite pwa plugin

--- src/app.tsx
+++ src/app.tsx
@@ -6,17 +6,17 @@ import Register from "@/pages/Register";
 import { ComponentShowcase } from "./components/component-showcase";
 
 export function App() {
-  return (
-    <ThemeProvider defaultTheme="light">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/showcase" element={<ComponentShowcase />} />
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider defaultTheme="light">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/showcase" element={<ComponentShowcase />} />
+                    <Route path="*" element={<div>404 Not Found</div>} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    )
 }
