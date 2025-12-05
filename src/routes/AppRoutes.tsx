f21e2100210d116353d8cec4fccfe14cdc90ec0d
pedroluanx28 <macielpedro794@gmail.com>
2025-12-05 16:15:53 -0300

    images

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,25 +1,42 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import { ComponentShowcase } from "@/components/component-showcase";
 import { PrivateRoutes } from "./PrivateRoutes";
-import { PrivateRoutesLayout } from "@/components/Layouts/PrivateRoutesLayout";
 
 export function AppRoutes() {
-    // Todo: Implement authentication check to conditionally render PrivateRoutes
+    function getStoredUser(): User | null {
+        const userStr = localStorage.getItem("user");
+        if (!userStr) {
+            return null;
+        }
+
+        try {
+            const user: User | null = userStr ? JSON.parse(userStr) : null;
+
+            return user;
+        } catch {
+            return null;
+        }
+    }
+
+    const isAuthenticated = getStoredUser() !== null;
+
     return (
         <Router>
             <Routes>
-                {/* <Route path="/*" element={<Navigate to="/login" />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/showcase" element={<ComponentShowcase />} /> */}
-
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/*" element={<PrivateRoutes />} />
-                <Route path="/showcase" element={<ComponentShowcase />} />
+                {isAuthenticated ? (
+                    <>
+                        <Route path="/*" element={<PrivateRoutes />} />
+                        <Route path="/showcase" element={<ComponentShowcase />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </>
+                )}
             </Routes>
         </Router>
     )
