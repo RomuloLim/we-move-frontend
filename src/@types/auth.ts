0fbb8e42f92ac03294d50f1b168ed1df474f9812
RomuloLim <romulo.lf123@gmail.com>
2025-12-06 16:32:50 -0300

    feat: edit profile

--- src/@types/auth.ts
+++ src/@types/auth.ts
@@ -21,8 +21,14 @@ type User = {
   id: number
   name: string
   email: string
+  cpf: string
+  rg: string | null
   gender: Gender
+  gender_label: string
   user_type: UserType
+  user_type_label: string
+  phone_contact: string
+  profile_picture_url: string | null
   student_profile?: Student
   email_verified_at: string | null
   created_at: string
@@ -44,6 +50,17 @@ type RegisterResponse = {
 }
 
 type MeResponse = {
+  data: User
+}
+
+type UpdateUserRequest = {
+  email?: string
+  password?: string
+  phone_contact?: string
+}
+
+type UpdateUserResponse = {
+  message: string
   data: User
 }
 
